0d4e17447b50ebc4c76b6523f9379fc4a6a19db2
AusBruce <113293507+AusBruce@users.noreply.github.com>
2025-03-05 18:03:09 +1100

    update application settings

---
+++ wild-oasis/src/features/cabins/AddCabin.jsx
@@ -0,0 +1,14 @@
+function AddCabin() {
+    
+const [showForm, setShowForm] = useState(false);
+  return (
+    <div>
+
+<Button onClick={()=>setShowForm(show=>!show)}>Add new Cabin</Button>
+      {showForm && <CreateCabinForm/>}
+
+    </div>
+  )
+}
+
+export default AddCabin
