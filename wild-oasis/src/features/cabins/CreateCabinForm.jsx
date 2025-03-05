fc6715cebd2e4427f751ca633dee92a1facc1665
AusBruce <113293507+AusBruce@users.noreply.github.com>
2025-03-05 18:28:03 +1100

    create modal compoment to add cabin

--- wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -8,7 +8,7 @@ import FormRow from "../../ui/FormRow";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({cabinToEdit ={}}) {
+function CreateCabinForm({cabinToEdit ={},onCloseModal}) {
 
   const {isCreating,createCabin} = useCreateCabin();
 
@@ -36,12 +36,16 @@ function CreateCabinForm({cabinToEdit ={}}) {
       {newCabinData:{ ...data, image },id:editId},{
         onSuccess: (data) => {
           console.log(data);
-          reset()},}
+          reset();
+          onCloseModal?.();
+        },}
     );
     else createCabin({ ...data, image: image },{
       onSuccess: (data) => {
         console.log(data);
-        reset()},
+        reset();
+      onCloseModal?.();
+      },
     });
    
 
@@ -54,8 +58,12 @@ function CreateCabinForm({cabinToEdit ={}}) {
     // console.log(errors);
   }
 
+
+
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form onSubmit={handleSubmit(onSubmit, onError)}
+    type ={onCloseModal? "modal" : "regular"}
+    >
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
           type="text"
@@ -147,7 +155,8 @@ function CreateCabinForm({cabinToEdit ={}}) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" 
+        onClick={ () => onCloseModal?.()}>
           Cancel
         </Button>
         <Button disabled={isWorking}>{isEditSession? 'Edit cabin' : 'Create new cabin'}</Button>
