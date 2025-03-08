6fa7c0e1b990f15190ba63402ace1e7f127348a0
AusBruce <113293507+AusBruce@users.noreply.github.com>
2025-03-08 13:59:53 +1100

    react compound component to wrap reusable menu items

--- wild-oasis/src/features/cabins/CabinTable.jsx
+++ wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,19 +1,10 @@
 import styled from "styled-components";
+
 import Spinner from "../../ui/Spinner";
-import CabinRow from "./CabinRow"; // Adjust the import path as necessary
-import { useCabins } from "./useCabins"; // Adjust the import path as necessary
-
-
-
-
-const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
-
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
-`;
+import CabinRow from "./CabinRow";
+import { useCabins } from "./useCabins";
+import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 const TableHeader = styled.header`
   display: grid;
@@ -30,38 +21,30 @@ const TableHeader = styled.header`
   padding: 1.6rem 2.4rem;
 `;
 
-
-
-
 function CabinTable() {
+  const { isLoading, cabins } = useCabins();
 
-  const{isLoading,cabins} = useCabins();
-  
-
-  if (isLoading) return <Spinner/>
-
+  if (isLoading) return <Spinner />;
 
   return (
-    <Table>
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+        <Table.Header>
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
 
-      <TableHeader role="table">
-        <div></div>
-        <div>Cabin</div>
-        <div>Capacity</div>
-        <div>Price</div>
-        <div>Discount</div>
-       <div></div>
-      
-      </TableHeader>
-
-      {cabins.map((cabin) => (
-     
-          <CabinRow cabin={cabin} key={cabin.id}/>
-     
-      ))}
-
-    </Table>
-  )
+        <Table.Body
+          data={cabins}
+          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+        />
+      </Table>
+    </Menus>
+  );
 }
 
-export default CabinTable
+export default CabinTable;
