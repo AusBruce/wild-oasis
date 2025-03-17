756882cbab8bf3313c6d8aedeac86db65d04bc0c
AusBruce <113293507+AusBruce@users.noreply.github.com>
2025-03-17 23:22:48 +1100

    filter cabin

---
+++ wild-oasis/src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,21 @@
+import TableOperations from "../../ui/TableOperations";
+
+import Filter from "../../ui/Filter";
+
+
+function CabinTablesOperations() {
+    return (
+        <TableOperations>
+          
+            <Filter filterField="discount" options={[
+                {value:'all', label:'All'},
+                {value:'no-discount', label:'No discount'},
+                {value:'with-discount', label:'With discount'}
+            ]} />
+            
+        </TableOperations>
+      );
+    }
+
+
+export default CabinTablesOperations
--- wild-oasis/src/pages/Cabins.jsx
+++ wild-oasis/src/pages/Cabins.jsx
@@ -2,6 +2,7 @@ import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 
 function Cabins() {
@@ -12,8 +13,8 @@ function Cabins() {
     <>
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>Filter/Sort</p>
-      
+      <CabinTableOperations/>
+
     </Row>
 
     <Row>
