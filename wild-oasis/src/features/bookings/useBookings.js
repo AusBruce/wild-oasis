e5bf17aaf0f83941b59c4cc06ebe737d0658faf9
AusBruce <113293507+AusBruce@users.noreply.github.com>
2025-03-21 14:43:40 +1100

    implement filter use reactQuery

--- wild-oasis/src/features/bookings/useBookings.js
+++ wild-oasis/src/features/bookings/useBookings.js
@@ -1,16 +1,27 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings'; // Adjust the import path as necessary
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
 
+  const [searchParams] = useSearchParams();
 
+  //Filter
+  const filterValue = searchParams.get("status")
+
+  const filter = !filterValue || filterValue === "all" 
+  ? null : {field: "status", value: filterValue};
+
+  // sortBy 
+
+  const sortBy =
 const {
     isLoading,
     data:bookings,
     error
 }= useQuery({
-    queryKey:['bookings'],
-    queryFn: getBookings,
+    queryKey:['bookings',filter],
+    queryFn: ()=>getBookings({filter}),
   });
 
   return {
--- wild-oasis/src/ui/Filter.jsx
+++ wild-oasis/src/ui/Filter.jsx
@@ -60,7 +60,7 @@ function Filter({filterField, options}) {
         <FilterButton key={option.value} 
         onClick={() => handleClick(option.value)}
         $active= {option.value === currentFilter  } 
-        disabled= {option.value === currentFilter  } 
+        $disabled= {option.value === currentFilter  } 
          > {option.label}
           
         </FilterButton>
